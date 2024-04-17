81529e454aec7c63de8b3aa0511ad0e1ca222152
higashi <higashi.y227@bmail.com>
2024-04-17 13:46:54 +0900

    commit2

--- src/main/java/servlet/IndexServlet.java
+++ src/main/java/servlet/IndexServlet.java
@@ -12,14 +12,14 @@ import jakarta.servlet.http.HttpServletResponse;
 @WebServlet("/IndexServlet")
 public class IndexServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
-protected void doGet(HttpServletRequest request,
-		HttpServletResponse response)
-		throws ServletException,IOException{
-	
-	RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
 
-	dispatcher.forward(request, response);
-}
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response)
+					throws ServletException,IOException{
+
+		RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
+
+		dispatcher.forward(request, response);
+	}
 
 }
