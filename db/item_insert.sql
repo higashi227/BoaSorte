da65f72bc1bb74e8ad4aee8b6095bc2dd8a2b6fe
M.Miyahara <minami.miyahara@gmail.com>
2024-04-17 11:07:27 +0900

    Merge branch 'main' of https://github.com/higashi227/BoaSorte.git into main

---
+++ db/item_insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO boasorte.item (name, price, is_coffee) VALUES ('ブルーマウンテン', 1297, 1);
+INSERT INTO boasorte.item (name, price, is_coffee) VALUES ('キリマンジャロ', 1112, 1);
+INSERT INTO boasorte.item (name, price, is_coffee) VALUES ('コナ', 926, 1);
+INSERT INTO boasorte.item (name, price, is_coffee) VALUES ('ドーナツ', 463, 0);
+INSERT INTO boasorte.item (name, price, is_coffee) VALUES ('フロランタン', 556, 0);
+INSERT INTO boasorte.item (name, price, is_coffee) VALUES ('クッキー', 364, 0);
+
+
+
