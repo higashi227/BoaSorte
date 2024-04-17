9c725be25a286c91abb33a90be11344c2e1e87e4
higashi <higashi.y227@bmail.com>
2024-04-17 16:17:01 +0900

    commit

---
+++ src/main/java/model/Item.java
@@ -0,0 +1,60 @@
+package model;
+
+public class Item {
+	private int id;
+	private String name; 
+	private int price;
+	private int is_coffee;
+
+
+	// コンストラクタ
+	public Item(int id, String name, int price, int is_coffee) {
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.is_coffee = is_coffee;
+	}
+
+	
+	
+	//IDのgetter.setterメソッド
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	
+	
+	//商品名のgetter.setterメソッド
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	
+	
+	//金額のgetter.setterメソッド
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	
+	
+	//コーヒーかどうかのgetter.setter
+	public int getIs_coffee() {
+		return is_coffee;
+	}
+	public void setIs_coffee(int is_coffee) {
+		this.is_coffee = is_coffee;
+	}
+
+
+}
